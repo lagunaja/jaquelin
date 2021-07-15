18ec8d3e12476fcec7fa64968db7b6721325fbc0
lagunaja <87486599+lagunaja@users.noreply.github.com>
2021-07-15 10:15:13 -0500

    Create code.java

---
+++ code.java
@@ -0,0 +1,14 @@
+public clas holis{
+  
+}
+public class holis:) {
+  public static void main (String[]args) {
+  
+  }
+  
+}
+public class holis:) {
+  public static void main (String[] args){
+    System.out.println("holis");
+  }
+}
